f01e77d44735856f7339ecb1b5d78d1d714b90a7
lucaspp19 <lucaspp191@gmail.com>
2023-06-25 13:41:52 -0300

    Project created

---
+++ src/main/java/com/javaProject/JavaSpringBootWeb/JavaSpringBootWebApplication.java
@@ -0,0 +1,13 @@
+package com.javaProject.JavaSpringBootWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaProject/JavaSpringBootWeb/JavaSpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaProject.JavaSpringBootWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
