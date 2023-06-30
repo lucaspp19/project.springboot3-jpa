0679816a5e0b70e780b8095d0f48866070963881
lucaspp19 <lucaspp191@gmail.com>
2023-06-30 16:47:41 -0300

    Exception handling - delete

--- src/main/java/com/javaProject/JavaSpringBootWeb/services/UserService.java
+++ src/main/java/com/javaProject/JavaSpringBootWeb/services/UserService.java
@@ -1,12 +1,16 @@
 package com.javaProject.JavaSpringBootWeb.services;
 
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.javaProject.JavaSpringBootWeb.entities.User;
+import com.javaProject.JavaSpringBootWeb.exceptions.DatabaseException;
 import com.javaProject.JavaSpringBootWeb.exceptions.ResourceNotFoundException;
 import com.javaProject.JavaSpringBootWeb.repositories.UserRepository;
 
@@ -35,9 +39,25 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		
+		try {
 		repository.deleteById(id);
+		
+		
+		}
+		/*catch(EmptyResultDataAccessException e) {
+			
+			throw new ResourceNotFoundException(id);
+			
+		}*/catch(DataIntegrityViolationException e) {
+			
+			throw new DatabaseException(e.getMessage());
+			
+		}catch(RuntimeException e) {
+		
+		e.printStackTrace();
 	}
+
+}
 	
 	public User Update(Long id, User obj) {
 		User entity = repository.getReferenceById(id);
